ea0a35e9f3990450bcf21d90612e1468cecebd98
KimWooSung <kipsong133@gmail.com>
2022-11-13 20:44:56 +0900

    first commit

---
+++ Fland-iOS-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fland-iOS-Project
+//
+//  Created by 김우성 on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
