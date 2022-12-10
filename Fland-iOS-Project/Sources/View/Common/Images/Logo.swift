bd9d7159988014193b8eab388bc93979900ebe1b
KimWooSung <kipsong133@gmail.com>
2022-12-10 18:57:09 +0900

    Feat: Font System 적용

---
+++ Fland-iOS-Project/Sources/View/Common/Images/Logo.swift
@@ -0,0 +1,20 @@
+//
+//  Logo.swift
+//  Fland-iOS-Project
+//
+//  Created by 김우성 on 2022/11/28.
+//
+
+import UIKit
+
+let logoImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "logo")
+    return imageView
+}()
+
+let handImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "handIcon")
+    return imageView
+}()
--- Fland-iOS-Project/Sources/View/SplashViewController.swift
+++ Fland-iOS-Project/Sources/View/SplashViewController.swift
@@ -6,20 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class SplashViewController: UIViewController {
-    
     // MARK: - Properties
+    let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "프랜드에 오신 것을\n환영합니다!"
+        label.numberOfLines = 2
+        label.font = UIFont.spoqa(26, .bold)
+        return label
+    }()
     
-    // MARK: - Initialize
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        
-    }
+    let welcomeSubLabel: UILabel = {
+        let label = UILabel()
+        label.text = "업무와 수입을 자유롭게 관리해보세요."
+        label.font = UIFont.spoqa(14, .medium)
+        return label
+    }()
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    let appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.setTitle("Apple로 로그인", for: .normal)
+        button.titleLabel?.setTextWithLineHeight("Apple로 로그인")
+        return button
+    }()
 }
 
 // MARK: - View Lifecycle
@@ -40,7 +52,45 @@ extension SplashViewController {
 extension SplashViewController {
     
     func setupLayout() {
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        
+        [
+            logoImageView,
+            handImageView,
+            welcomeLabel,
+            welcomeSubLabel,
+            appleLoginButton
+        ].forEach { view.addSubview($0)}
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(25)
+            make.leading.equalTo(view.snp.leading).offset(20)
+        }
+        
+        handImageView.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(55)
+            make.leading.equalTo(logoImageView.snp.leading)
+        }
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalTo(handImageView.snp.bottom).offset(5)
+            make.leading.equalTo(logoImageView.snp.leading)
+        }
+        
+        welcomeSubLabel.snp.makeConstraints { make in
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(5)
+            make.leading.equalTo(logoImageView.snp.leading)
+        }
+        
+        appleLoginButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.snp.bottom).inset(34)
+            make.centerX.equalTo(view.snp.centerX)
+            make.leading.equalTo(view.snp.leading).offset(20)
+            make.trailing.equalTo(view.snp.trailing).inset(20)
+            make.height.equalTo(56)
+        }
+        
+        
     }
 }
 
